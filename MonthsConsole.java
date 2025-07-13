39932bfe8b93eec5f0949b57cfaaf0e3d861086c
tay4ik2451 <fortniter2470@gmail.com>
2025-07-13 13:05:47 +0300

    Add files via upload

---
+++ MonthsConsole.java
@@ -0,0 +1,9 @@
+package homework2;
+
+public class MonthsConsole {
+    public static void main(String[] args) {
+        for (Months month : Months.values()) {
+            System.out.println("Месяц : " + month);
+        }
+    }
+}
